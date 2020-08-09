54b45c1cc55b9da352436d5c3b17726494f92f36
Angelo Milien <angelomilienjoseph@gmail.com>
2020-08-09 16:38:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
 }
 
 
-function updatePlaylist (obj, string, title) {
+function updatePlaylist (playlist, artistName, songTitle) {
   playlist["garou"] = "je l aime"
   return playlist
 }
