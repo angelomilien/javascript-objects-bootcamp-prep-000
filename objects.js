1f556807f053e9a1f27260a5e13f5586d3951735
Angelo Milien <angelomilienjoseph@gmail.com>
2020-08-09 16:45:50 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,3 +7,8 @@ function updatePlaylist (playlist, artistName, songTitle) {
   playlist["Phil Ochs"] = "Slowdive"
   return playlist
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
